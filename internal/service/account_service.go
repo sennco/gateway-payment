864ddd37342128cb97bd215d4ab961c66dcda470
João <joao@senco.dev.br>
2025-04-10 16:54:27 -0300

    first commit

---
+++ internal/service/account_service.go
@@ -0,0 +1,71 @@
+package service
+
+import (
+	"github.com/sennco/gateway-payment/internal/domain"
+	"github.com/sennco/gateway-payment/internal/dto"
+)
+
+type AccountService struct {
+	repository domain.AccountRepository
+}
+
+func NewAccountService(repository domain.AccountRepository) *AccountService {
+	return &AccountService{repository: repository}
+}
+
+func (s *AccountService) CreateAccount(input *dto.CreateAccountInput) (*dto.AccountOutput, error) {
+	account := dto.ToAccount(input)
+	
+	existingAccount, err := s.repository.FindByAPIKey(account.APIKey)
+	if err != nil && err != domain.ErrAccountNotFound {
+		return nil, err
+	}
+	if existingAccount != nil {
+		return nil, domain.ErrDuplicatedAPIKey
+	}
+
+	err = s.repository.Save(account)
+	if err != nil {
+		return nil, err
+	}
+
+	output := dto.FromAccount(account)
+
+	return output, nil
+
+}
+
+func (s *AccountService) UpdateBalance(apiKey string, amount float64) (*dto.AccountOutput, error) {
+	account, err := s.repository.FindByAPIKey(apiKey)
+	if err != nil {
+		return nil, err
+	}
+
+	account.AddBalance(amount)
+	err = s.repository.UpdateBalance(account)
+	if err != nil {
+		return nil, err
+	}
+
+	output := dto.FromAccount(account)
+
+	return output, nil
+}
+
+func (s *AccountService) FindByAPIKey(apiKey string) (*dto.AccountOutput, error) {
+	account, err := s.repository.FindByAPIKey(apiKey)
+	if err != nil {
+		return nil, err
+	}
+
+	return dto.FromAccount(account), nil
+}
+
+func (s *AccountService) FindByID(id string) (*dto.AccountOutput, error) {
+	account, err := s.repository.FindByID(id)
+	if err != nil {
+		return nil, err
+	}
+
+	return dto.FromAccount(account), nil
+}
